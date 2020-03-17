5e3910445a7a3abd036c7b0357b143f7f61f896b
theslickestnick <nick.echev@gmail.com>
2020-03-16 20:37:52 -0400

    Done.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+
 RSpec.describe Post, type: :model do
   let(:content) { "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus, nulla vel condimentum ornare, arcu lorem hendrerit purus, ac sagittis ipsum nisl nec erat. Morbi porta sollicitudin leo, eu cursus libero posuere ac. Sed ac ultricies ante. Donec nec nulla ipsum. Nunc eleifend, ligula ut volutpat." }
 
