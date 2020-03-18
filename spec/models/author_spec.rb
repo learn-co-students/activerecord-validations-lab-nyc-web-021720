9e8dc864b47ff50fc7c7ad23d5ce47c24c47915f
JohnsonNYC <jkow95@gmail.com>
2020-03-18 15:11:19 -0400

    Done.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -15,10 +15,11 @@ RSpec.describe Author, type: :model do
     author = Author.new(name: "Caligula", phone_number: 555305442)
     expect(author).to be_invalid
   end
-
+ 
   it "is invalid when non-unique" do
     Author.create(name: "Caligula", phone_number: 5553054425)
     author = Author.new(name: "Caligula", phone_number: 5557890001)
     expect(author).to be_invalid
   end
 end
+
